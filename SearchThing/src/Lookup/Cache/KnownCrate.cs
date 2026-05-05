421c968d0f328378faa31ef825eafe718984877a
mash <mash@mashedram.dev>
2026-05-05 12:53:45 +0200

    - Known crate struct

--- SearchThing/src/Lookup/Cache/KnownCrate.cs
+++ SearchThing/src/Lookup/Cache/KnownCrate.cs
@@ -1,3 +1,6 @@
+using Il2CppSLZ.Marrow.Warehouse;
+using SearchThing.Search;
+
 namespace SearchThing.Lookup.Cache;
 
 public class KnownCrate
@@ -6,17 +9,22 @@ public class KnownCrate
     public string Name { get; set; }
     public string Description { get; set; }
     public string[] Tags { get; set; }
-    public KnownPallet Pallet { get; set; } 
-}
+    
+    public string PalletName { get; set; }
+    public string PalletAuthor { get; set; }
+    
+    public int ModId { get; set; }
+    public int FileId { get; set; }
+    
+    public KnownCrate() {}
 
-public class KnownPallet
-{
-    public string Barcode { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Author { get; set; }
-    
-    public string DownloadLink { get; set; }
-    
-    public List<KnownCrate> Crates { get; set; }
+    public KnownCrate(ISearchableCrate searchableCrate)
+    {
+        Barcode = searchableCrate.Barcode._id;
+        Name = searchableCrate.Name.Original;
+        Description = searchableCrate.Description;
+        Tags = searchableCrate.Tags.Select(t => t.Original).ToArray();
+        
+        
+    }
 }
